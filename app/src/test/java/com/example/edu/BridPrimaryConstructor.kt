32be653cac7b43f1539c6d3a76c14fa03cba5cc8
ddoridori <godltjgml2@naver.com>
2021-05-22 16:40:51 +0900

    5단원복습중

--- app/src/test/java/com/example/edu/BridPrimaryConstructor.kt
+++ app/src/test/java/com/example/edu/BridPrimaryConstructor.kt
@@ -1,2 +1,19 @@
 package com.example.edu
 
+class Birdd(var name:String, var  wing: Int, var beak: String, var color: String) {
+
+    //var사용으로 _,this,construcotr 생략
+
+    fun fly() = println("Fly wing : $wing")
+    fun sing(vol: Int) = println("Sing vol: $vol")
+
+}
+
+fun main() {
+    val coco = Bird("Mybird", 2,"short","blue")
+
+    coco.color = "yellow"
+    println("coco.color: ${coco.color}")
+    coco.fly()
+    coco.sing(3)
+}
